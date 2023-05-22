27161bf51b286b576dddd271ac067fcf84d6ff9e
Arham Shams <muhammad.arham1019@gmail.com>
2023-05-22 11:56:16 +0500

    new changes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,26 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
+
+
+interface IlocationListDto{
+  locations: string[];
+}
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Render('list.hbs')
+  listlocations(): IlocationListDto {
+    return {
+      locations:[
+        'location:1',
+        'location:2',
+        'location:3',
+        'location:4',
+        'location:5'
+      ],
+    };
   }
 }
